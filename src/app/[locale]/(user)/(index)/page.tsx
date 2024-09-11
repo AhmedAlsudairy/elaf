c6533666c1e1907fca690865e51be8cc323616db
Ahmed Khalifa Khamis Al Sideiri <94130258+AhmedAlsudairy@users.noreply.github.com>
2024-09-12 00:18:43 +0400

    Update page.tsx

--- src/app/[locale]/(user)/(index)/page.tsx
+++ src/app/[locale]/(user)/(index)/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import LandingPage from "@/components/pages/user/LandingPage/landing-page";
 
 export const metadata: Metadata = {
-  title: "Your Landing Page Title",
-  description: "Your Landing Page Description",
+  title: "Elaf tenders",
+  description: "Elaf tender platform main page",
 };
 
 const Page = ({ searchParams }: { searchParams: { msg: string | undefined } }) => {
